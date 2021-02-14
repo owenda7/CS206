561ef107f7f332acb15ff580450966d261b6e8bc
owenda7 <owenda7@gmail.com>
2021-02-14 16:49:13 -0500

    manylinks

--- generate.py
+++ generate.py
@@ -1,5 +1,25 @@
 import pyrosim.pyrosim as pyrosim
 
+length = 1
+width = 1
+height = 1
+
+x = 0
+y = 0
+z = .5
+
 pyrosim.Start_SDF("box.sdf")
-pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[1,1,1])
+for x in range(0,6):
+    for y in range(0,6):
+        length = 1
+        width = 1
+        height = 1
+        z = .5
+        for k in range(0,10):
+            pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length,width,height])
+            z = z+height
+            length = length * .9
+            width = width * .9
+            height = height * .9
+
 pyrosim.End()

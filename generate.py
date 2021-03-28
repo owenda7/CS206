8af45639c86dcde1be41e4ef65053b614ae0450d
owenda7 <owenda7@gmail.com>
2021-03-28 16:17:29 -0400

    final random search

--- generate.py
+++ generate.py
@@ -1,6 +1,7 @@
 import pyrosim.pyrosim as pyrosim
 import constants as c
 import numpy
+import random
 
 def Create_World():
     pyrosim.Start_SDF("world.sdf")
@@ -23,10 +24,9 @@ def Generate_Brain():
     pyrosim.Send_Sensor_Neuron(name = 2, linkName = "FrontLeg")
     pyrosim.Send_Motor_Neuron(name = 3, jointName= "Torso_BackLeg")
     pyrosim.Send_Motor_Neuron(name = 4, jointName= "Torso_FrontLeg")
-    pyrosim.Send_Synapse( sourceNeuronName = 0, targetNeuronName = 3, weight = -10.0 ) 
-    pyrosim.Send_Synapse( sourceNeuronName = 1, targetNeuronName = 3, weight = -10.0 ) 
-    pyrosim.Send_Synapse( sourceNeuronName = 0, targetNeuronName = 4, weight = -10.0 ) 
-    pyrosim.Send_Synapse( sourceNeuronName = 2, targetNeuronName = 4, weight = -10.0 )
+    for i in range(0,3):
+        for j in range(3,5):
+            pyrosim.Send_Synapse( sourceNeuronName = i, targetNeuronName = j, weight = random.random() )
     pyrosim.End()
     
 
